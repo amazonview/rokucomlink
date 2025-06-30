a4a47bebf4b65c3e9277d2692786e1b39d74036a
amazonview <chetijasimran@gmail.com>
2025-06-30 18:48:00 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'Roku Link'
 
 
 # The full version, including alpha/beta/rc tags
